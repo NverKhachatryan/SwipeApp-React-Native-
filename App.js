46a4312b78eab7fab7c216e67a369b526c3142e3
Nver Khachatryan <78234170+NverKhachatryan@users.noreply.github.com>
2021-12-22 00:59:23 +0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,181 +1,89 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 
-const SCREEN_HEIGHT = Dimensions.get('window').height
-const SCREEN_WIDTH = Dimensions.get('window').width
-import Icon from 'react-native-vector-icons/Ionicons'
-const Users = [
-  { id: "1", uri: require('./assets/1.jpeg') },
-  { id: "2", uri: require('./assets/2.jpeg') },
-  { id: "3", uri: require('./assets/3.jpeg') },
-  { id: "4", uri: require('./assets/4.jpeg') },
-  { id: "5", uri: require('./assets/5.jpeg') },
-]
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
-export default class App extends React.Component {
+export default class App extends React.Component{
+  
+  state = { 
+    hasLocationPermission: false,
+    latitude: 0,
+    longitude: 0,
+    restaurantList: []
+  }
+  componentDidMount() {
+    this.getLocationAsync();
+  };
+  
+  async getLocationAsync () {
+    const { status } = await Permissions.askAsync(
+      Permissions.LOCATION
+    );
+    
+    if (status === 'granted') {
+      let location = await Location.getCurrentPositionAsync({});
 
-  constructor() {
-    super()
-
-    this.position = new Animated.ValueXY()
-    this.state = {
-      currentIndex: 0
+      this.setState({
+        hasLocationPermissions: true,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    } else {
+      alert('Location permission not granted');
     }
+    
+  };
 
-    this.rotate = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH /2 ,0, SCREEN_WIDTH /2],
-      outputRange: ['-30deg', '0deg', '10deg'],
-      extrapolate: 'clamp'
+  handleRestaurantSearch = () => {
+    const location = `location=${this.state.latitude},${this.state.longitude}`; 
+    
+    
+    fetch(`https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${this.state.latitude}&longitude=${this.state.longitude}&limit=30&currency=USD&distance=2000&open_now=false&lunit=km&lang=en_US`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": "66793091c5mshccead1a3d06a524p1059b8jsnc99793b18717"
+      }
     })
+      
+      .then(response => response.json())
+      .then(results => this.setState({restaurantList: results}))
 
-    this.rotateAndTranslate = {
-      transform: [{
-        rotate: this.rotate
-      },
-      ...this.position.getTranslateTransform()
-      ]
-    }
-
-    this.likeOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [0, 0, 1],
-      extrapolate: 'clamp'
-    })
-    this.dislikeOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [1, 0, 0],
-      extrapolate: 'clamp'
-    })
-
-    this.nextCardOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [1, 0, 1],
-      extrapolate: 'clamp'
-    })
-    this.nextCardScale = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [1, 0.8, 1],
-      extrapolate: 'clamp'
-    })
-
+      .catch(err => {
+        console.error(err);
+      });
   }
-  UNSAFE_componentWillMount() {
-    this.PanResponder = PanResponder.create({
-
-      onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onPanResponderMove: (evt, gestureState) => {
-
-        this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
-      },
-      onPanResponderRelease: (evt, gestureState) => {
-
-        if (gestureState.dx > 120) {
-          Animated.spring(this.position, {
-            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy },
-            useNativeDriver: true,
-          }).start(() => {
-            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
-              this.position.setValue({ x: 0, y: 0 })
-            })
-          })
-        }
-        else if (gestureState.dx < -120) {
-          Animated.spring(this.position, {
-            toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy },
-            useNativeDriver: true,
-          }).start(() => {
-            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
-              this.position.setValue({ x: 0, y: 0 })
-            })
-          })
-        }
-        else {
-          Animated.spring(this.position, {
-            toValue: { x: 0, y: 0 },
-            friction: 4,
-            useNativeDriver: true,
-          }).start()
-        }
-      }
-    })
-  }
-
-  renderUsers = () => {
-
-    return Users.map((item, i) => {
-
-
-      if (i < this.state.currentIndex) {
-        return null
-      }
-      else if (i == this.state.currentIndex) {
-
-        return (
-          <Animated.View
-            {...this.PanResponder.panHandlers}
-            key={item.id} style={[this.rotateAndTranslate, { height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
-            <Animated.View style={{ opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
-
-            </Animated.View>
-
-            <Animated.View style={{ opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
-
-            </Animated.View>
-
-            <Image
-              style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
-              source={item.uri} />
-
-          </Animated.View>
-        )
-      }
-      else {
-        return (
-          <Animated.View
-
-            key={item.id} style={[{
-              opacity: this.nextCardOpacity,
-              transform: [{ scale: this.nextCardScale }],
-              height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute'
-            }]}>
-            <Animated.View style={{ opacity: 0, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
-
-            </Animated.View>
-
-            <Animated.View style={{ opacity: 0, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
-
-            </Animated.View>
-
-            <Image
-              style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
-              source={item.uri} />
-
-          </Animated.View>
-        )
-      }
-    }).reverse()
-  }
-
+ 
   render() {
+    console.log(this.state.restaurantList)
     return (
-      <View style={{ flex: 1 }}>
-        <View style={{ height: 60 }}>
-
-        </View>
-        <View style={{ flex: 1 }}>
-          {this.renderUsers()}
-        </View>
-        <View style={{ height: 60 }}>
-
-        </View>
-
-
+      <View style={this.styles.container}>
+        <FlatList  
+          data={this.state.restaurantList}
+          renderItem={({item}) => (
+            <Text>{item.name}</Text>
+          )}
+          
+          
+          style={{backgroundColor: 'grey', width: '80%', margin: 60, padding: 5}}
+        />
+        <TouchableOpacity onPress={() => this.handleRestaurantSearch()}>
+          <Text style={{backgroundColor: 'grey', color: 'white', padding: 20, marginBottom: 50}}>Search Restaurants</Text>
+        </TouchableOpacity>
+        
+        <StatusBar style="auto" />
       </View>
-
     );
   }
+
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 }
